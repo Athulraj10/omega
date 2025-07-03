0e4627ccc61c3178827b00795c600f7691c45dfb
athulraj10 <athulraj165@gmail.com>
2025-07-03 11:12:00 +0530

    initial

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+
+import Providers from "@/store/Provider";
+import { Loader } from "@/components/loader";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export const metadata = {
+  title: "Omega",
+  description: "Export Import company",
+
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body style={{ background: "none" }}>
+        <Loader>
+          <Providers>
+            <div>{children}</div>
+          </Providers>
+        </Loader>
+      </body>
+    </html>
+  );
+}
