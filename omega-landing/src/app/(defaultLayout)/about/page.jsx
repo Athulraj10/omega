2dd1d9be2a7fd1211e7a31cb58aa17a74f235245
Athulraj10 <athulraj165@gmail.com>
2026-01-11 19:30:14 +0530

    working on the about and footer page

--- omega-landing/src/app/(defaultLayout)/about/page.jsx
+++ omega-landing/src/app/(defaultLayout)/about/page.jsx
@@ -1,8 +1,6 @@
 import BreadCumb from '@/app/Components/Common/BreadCumb';
 import CtaBanner1 from '@/app/Components/CtaBanner/CtaBanner1';
-import CtaBanner3 from '@/app/Components/CtaBanner/CtaBanner3';
 import Gallery1 from '@/app/Components/Gallery/Gallery1';
-import Team3 from '@/app/Components/Team/Team3';
 import Testimonial2 from '@/app/Components/Testimonial/Testimonial2';
 import React from 'react';
 
@@ -14,8 +12,6 @@ const page = () => {
                 Title="About Us"
             ></BreadCumb>
             <CtaBanner1></CtaBanner1>
-            <Team3></Team3>
-            <CtaBanner3></CtaBanner3>
             <Testimonial2></Testimonial2>
             <Gallery1></Gallery1>            
     </div>
